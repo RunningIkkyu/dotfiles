b84b16ed0b6165ecf9c8811fed039fc028d4b9c6
lane <389498623@qq.com>
2021-06-04 16:41:11 +0800

    update intall shell

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config
 ln -s $PWD/nvim/ $HOME/.config/nvim
 
 cp ./.alias $HOME/
 cp ./.exports $HOME/
+
+echo "sh $HOME/.alias" >> $HOME/.zshrc
+echo "sh $HOME/.exports" >> $HOME/.zshrc
+
 #./bootstrap.sh
 
