4710480db72d8b45acd7a5064901562accfc99ce
ikkyu <389498623@qq.com>
2021-08-16 13:57:13 +0800

    add: install script for zsh

--- install.sh
+++ install.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DOT_FILE_LIST="zsh/.zshrc zsh/.alias zsh/.export"
+DST_DIR=$HOME
 
-mkdir -p $HOME/.config
-ln -s $PWD/nvim/ $HOME/.config/nvim
+force=$1
 
-cp ./.alias $HOME/
-cp ./.exports $HOME/
+if [ "$force" == "-f" ]; then
+    force=1
+else
+    force=0
+fi
 
-#./bootstrap.sh
+cd $DST_DIR
 
+for file in $DOT_FILE_LIST
+do
+    name=`basename $file`
+    #echo "$DST_DIR/$name" 
+    # back up file or remove file based on "-f" flag
+    if [ -e "$DST_DIR/$name" ]; then
+        if [ $force -eq 0 ]; then
+            mv "$name" "$name.bk"
+            echo "backup $PWD/$name to $PWD/$name.bk"
+        else
+            rm "$name"
+            echo "remove old config $PWD/$name"
+        fi
+    fi
+
+    cd $DST_DIR && ln -s $SCRIPT_DIR/$file .
+done
