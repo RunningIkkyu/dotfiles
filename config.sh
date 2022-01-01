25cf5c5fc10d8e0cb808c63640a9a329d0b0384c
ikkyu <389498623@qq.com>
2022-01-02 00:37:57 +0800

    docs: add README.md for neovim

--- config.sh
+++ config.sh
@@ -6,15 +6,48 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
 # Backup the old nvim config
-if [[ -e ~/.config/nvim ]]; then
-    mv ~/.config/nvim ~/.config/nvim.bak
+if [[ -e $HOME/.config/nvim ]]; then
+    mv $HOME/.config/nvim $HOME/.config/nvim.bak
 fi
 
 # Create the new nvim config
-mkdir -p ~/.config
-cd ~/.config && ln -s $DIR/nvim .
+if [[ ! -e $HOME/.confg ]]; then
+    mkdir $HOME/.config
+fi
+ln -s $DIR/nvim $HOME/.config/nvim
 
 # Install Packer.nvim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
      ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+
+# Install packer plugins
+nvim +PackUpdate +qall
+
+# Enumeration for operating system
+OS_LINUX=0
+OS_MAC=1
+OS_WIN=2
+OS_OTHER=3
+
+# Function to detect operating system
+function detect_os() {
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        return $OS_LINUX
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        return $OS_MAC
+    elif [[ "$OSTYPE" == "cygwin" ]]; then
+        return $OS_WIN
+    else
+        return $OS_OTHER
+}
+
+# Check if command is available
+function command_exists() {
+    command -v "$@" > /dev/null 2>&1
+}
+
+# Install neovim for mac.
+function install_neovim_for_mac() {
+    brew install neovim
+}
---
+++ nvim/README.md
@@ -0,0 +1,27 @@
+# Open to Use Neovim
+
+This is my neovim configuration.
+
+If you're looking for a portable version of vim which you could use like an IDE, this is for you.
+ 
+# Feature
+
+- Easy to install.
+- LSP support.
+- Awesome UI.
+- Include useful git tools.
+- Self defined key binding.
+- Copilot integreted.
+- Snippet support.
+- Fuzzy finder.
+- Easy to Extension.
+
+# Quick Start
+
+You need neovim >= 0.6.0 installed.
+
+Just one step to config your nvim.
+
+```
+```
+
